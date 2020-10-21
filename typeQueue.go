22c87a4531a10f8de276902c0078afe8ba36ede6
June Kim <godlyjune@gmail.com>
2020-10-21 20:24:35 +0900

    Add ref to amqp.Queue

--- typeQueue.go
+++ typeQueue.go
@@ -1,20 +1,25 @@
 package mqi
 
+import "github.com/streadway/amqp"
+
 // Queue struc
 type queue struct {
-	name string     // Name of Queue
-	tps  []Topic    // Binded topics
-	csms []Consumer // Binded consumers
+	name string      // Name of Queue
+	ref  *amqp.Queue // Reference to the attached AMQP Queue
+	tps  []Topic     // Binded topics
+	csms []Consumer  // Binded consumers
 }
 
 //Queue interface
 type Queue interface {
 	Name() string
+	Ref() *amqp.Queue
 	CountTopics() int
 	TopicAt(i int) Topic
 	CountConsumers() int
 	ConsumerAt(i int) Consumer
 	WithName(name string) Queue
+	WithRef(ref *amqp.Queue) Queue
 	AddTopic(tp Topic) Queue
 	AddConsumer(cs Consumer) Queue
 }
@@ -24,6 +29,9 @@ func NewQueue(name string) Queue { return queue{}.WithName(name) }
 
 // Name getter
 func (q queue) Name() string { return q.name }
+
+// Ref getter
+func (q queue) Ref() *amqp.Queue { return q.ref }
 
 // CountTopics counts topics
 func (q queue) CountTopics() int { return len(q.tps) }
@@ -40,6 +48,11 @@ func (q queue) ConsumerAt(i int) Consumer { return q.csms[i] }
 // WithName sets name
 func (q queue) WithName(name string) Queue {
 	q.name = name
+	return q
+}
+
+func (q queue) WithRef(ref *amqp.Queue) Queue {
+	q.ref = ref
 	return q
 }
 
